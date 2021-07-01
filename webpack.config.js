4e0d1492edff43c03afbde71f11a2f54627c88d0
JonMLunde <jonlund95@gmail.com>
2021-07-01 15:17:03 +0200

    webpack styling and html toolchain

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,53 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-var-requires */
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const devMode = process.env.NODE_ENV !== 'production';
 
 module.exports = {
-  mode: 'development',
   entry: './src/index.tsx',
   output: {
-    filename: 'bundle.js',
     path: path.resolve(__dirname, 'public'),
-    publicPath: '/public/',
+    filename: 'bundle.js',
   },
+  devtool: 'eval-source-map',
   devServer: {
     port: '3000',
+    watchContentBase: true,
   },
+  plugins: [new HtmlWebpackPlugin()],
   module: {
     rules: [
       {
         test: /\.tsx?$/,
         exclude: /node_modules/,
         loader: 'ts-loader',
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader', //  CSS -> JS
+          {
+            loader: 'postcss-loader', // Apply PostCSS
+            options: {
+              postcssOptions: {
+                plugins: [
+                  [
+                    'autoprefixer',
+                    {
+                      // Options
+                    },
+                  ],
+                ],
+              },
+            },
+          },
+          'sass-loader', // SASS -> CSS
+        ],
       },
     ],
   },
